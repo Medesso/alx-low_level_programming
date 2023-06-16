0ab591fa32a95a4f6c7c41d2a176c45e55686849
root <essouani.med@gmail.com>
2023-06-16 12:21:01 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print double digit combos
+ * main - print single digit numbers
  *
- * Description: print double digit combos
+ * Description: print single digit numbers with commas
  *
  * Return: Always 0 (Success)
  */
@@ -17,13 +17,13 @@ int main(void)
 
 	while (i < 58)
 	{
-		j = i + 1;
+		j = 48;
 		while (j < 58)
 		{
 			putchar(i);
 			putchar(j);
 
-			if (i < 56 || j < 57)
+			if (i < 57 || j < 57)
 			{
 				putchar(44);
 				putchar(32);
@@ -37,5 +37,4 @@ int main(void)
 	putchar(10);
 
 	return (0);
-}
-}
+}}
