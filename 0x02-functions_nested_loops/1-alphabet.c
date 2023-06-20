a053b69ada1ed48af6e94b4fe81d802157c6ae17
root <essouani.med@gmail.com>
2023-06-20 16:12:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "Betty"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
