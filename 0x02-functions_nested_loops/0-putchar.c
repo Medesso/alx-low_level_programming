cb386d6079619117f2d095ddb1c746b3cd43de62
root <essouani.med@gmail.com>
2023-06-20 17:21:51 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#inclu_putchar"
+#include "main.h"
 
 /**
  * main - prints putchar
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char text[10] = "putchar";
+		char text[10] = "putchar";
 	int i = 0;
 
 	for (i = 0; i < 9; i++)
